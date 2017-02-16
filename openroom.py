c9bb1785cc031b37fb9f120234bce9306585cf80
kushal <kushaldeveloper@gmail.com>
2017-02-16 16:16:16 -0500

    teach a dog new tricks
    add an array to instance of dog
    
    Signed-off-by: kushal <kushaldeveloper@gmail.com>

--- openroom.py
+++ openroom.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     fido = Dog.Dog('Fido')
-    return fido.name
+    fido.add_trick('roll over')
+    fido.add_trick('play dead')
+    return str(len(fido.tricks))
 
 
 if __name__ == '__main__':
