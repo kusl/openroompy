a82f33dbf18781165f81054092c94b8ffe952452
kushal <kushaldeveloper@gmail.com>
2017-02-17 13:13:47 -0500

    create a basic user
    Signed-off-by: kushal <kushaldeveloper@gmail.com>

---
+++ User.py
@@ -0,0 +1,35 @@
+import uuid
+
+import bcrypt
+import re
+
+
+class User(object):
+    MINIMUM_PASSWORD_LENGTH = 8
+    EMAIL_PATTERN = r"[^@]+@[^@]+\.[^@]+"
+
+    def __init__(self):
+        self.id = None
+        self.password = None
+        self.email = None
+
+    def create(self, password, email):
+        self.id = uuid.uuid4()
+        self.set_password(self, password=password)
+        self.set_email(self, email=email)
+        return self
+
+    def set_password(self, password):
+        if len(password) >= User.MINIMUM_PASSWORD_LENGTH:
+            self.password = str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode())
+
+    def set_email(self, email):
+        if re.match(User.EMAIL_PATTERN, email):
+            self.email = email
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'password': self.password,
+        }
--- openroom.py
+++ openroom.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from User import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 42
+    return "hello, world"
+
+
+@app.route('/api/v0/users', methods=['POST'])
+def create_user():
+    email = request.json['email']
+    password = request.json['password']
+    user = User
+    user.create(user, email=email, password=password)
+    return jsonify(user.serialize(user)), 201
 
 
 if __name__ == '__main__':
