d7584adc605d29822bdb5bdfeb57bb666122dba9
kushal <kushaldeveloper@gmail.com>
2017-02-19 16:25:14 -0500

    add sqlalchemy
    very WIP
    
    Signed-off-by: kushal <kushaldeveloper@gmail.com>

--- openroom.py
+++ openroom.py
@@ -1,8 +1,42 @@
+import bcrypt
+import re
 from flask import Flask, request, jsonify, url_for
-
-from User import User
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    MINIMUM_PASSWORD_LENGTH = 8
+    EMAIL_PATTERN = r"[^@]+@[^@]+\.[^@]+"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
+
+    def __init__(self, email, password):
+        self.set_email(email=email)
+        self.set_password(password=password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def set_password(self, password):
+        if len(password) >= User.MINIMUM_PASSWORD_LENGTH:
+            self.password = str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode())
+
+    def set_email(self, email):
+        if re.match(User.EMAIL_PATTERN, email):
+            self.email = email
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'password': self.password,
+        }
 
 
 @app.route('/')
@@ -14,8 +48,7 @@ def hello_world():
 def create_user():
     email = request.json['email']
     password = request.json['password']
-    user = User()
-    user.create(email=email, password=password)
+    user = User(email, password)
     return jsonify(user.serialize()), 201
 
 
